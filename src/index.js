ac9fd0cfc36fb5a6836b00e5fd1e66d65be32492
AlexisCast <castellanos.alexis@hotmail.com>
2023-08-22 15:01:33 -0700

    add update and delete endpoints

--- src/index.js
+++ src/index.js
@@ -47,6 +47,47 @@ app.post("/users", async (req, res) => {
 	}
 });
 
+app.patch("/users/:id", async (req, res) => {
+	const updates = Object.keys(req.body);
+	const allowedUpdates = ["name", "email", "password", "age"];
+	const isValidOperation = updates.every((update) => {
+		return allowedUpdates.includes(update);
+	});
+
+	if (!isValidOperation) {
+		return res.status(400).send({ error: "Invalid updates!" });
+	}
+
+	try {
+		const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+			new: true,
+			runValidators: true,
+		});
+
+		if (!user) {
+			return res.status(404).send();
+		}
+
+		res.send(user);
+	} catch (e) {
+		res.status(400).send(e);
+	}
+});
+
+app.delete("/users/:id", async (req, res) => {
+	try {
+		const user = await User.findByIdAndDelete(req.params.id);
+
+		if (!user) {
+			return res.status(400).send();
+		}
+
+		res.send(user);
+	} catch (e) {
+		res.status(500).send();
+	}
+});
+
 app.post("/tasks", async (req, res) => {
 	const task = new Task(req.body);
 
@@ -79,6 +120,46 @@ app.get("/tasks/:id", async (req, res) => {
 		res.send(task);
 	} catch (e) {
 		res.status(500).send();
+	}
+});
+
+app.patch("/tasks/:id", async (req, res) => {
+	const updates = Object.keys(req.body);
+	const allowedUpdates = ["description", "completed"];
+	const isValidOperation = updates.every((update) => {
+		return allowedUpdates.includes(update);
+	});
+
+	if (!isValidOperation) {
+		return res.status(400).send({ error: "Invalid Updates!" });
+	}
+
+	try {
+		const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
+			new: true,
+			runValidators: true,
+		});
+
+		if (!task) {
+			return res.status(404).send(task);
+		}
+
+		res.send(task);
+	} catch (e) {
+		res.status(400).send(e);
+	}
+});
+
+app.delete("/tasks/:id", async (req, res) => {
+	try {
+		const task = await Task.findByIdAndDelete(req.params.id);
+		if (!task) {
+			return res.status(400).send();
+		}
+
+		res.send(task);
+	} catch (e) {
+		res.status(500).send(e);
 	}
 });
 
