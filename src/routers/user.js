f595b2fd70a903cf747491f65f007e6dcef3b952
AlexisCast <castellanos.alexis@hotmail.com>
2023-08-23 14:12:55 -0700

    add Logging out

--- src/routers/user.js
+++ src/routers/user.js
@@ -3,6 +3,21 @@ const User = require("../models/user");
 const auth = require("../middleware/auth");
 const router = new express.Router();
 
+router.post("/users/logout", auth, async (req, res) => {
+	try {
+		req.user.tokens = req.user.tokens.filter((token) => {
+			console.log(`${token.token} ---- ${req.token}`)
+			return token.token !== req.token;
+		});
+		
+		await req.user.save();
+
+		res.send();
+	} catch (e) {
+		res.status(500).send();
+	}
+});
+
 router.get("/users/me", auth, async (req, res) => {
 	res.send(req.user);
 });
