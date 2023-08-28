0b32cf8143a8e5167915beadf7e956a10a37adb8
AlexisCast <castellanos.alexis@hotmail.com>
2023-08-28 14:32:03 -0700

    send welcome and cancelation email

--- src/emails/account.js
+++ src/emails/account.js
@@ -1,26 +1,27 @@
 const nodemailer = require("nodemailer");
 
-const sendMail = () => {
-	console.log("sendemail");
-	const transporter = nodemailer.createTransport({
-		service: "gmail",
-		auth: {
-			user: process.env.NODEMAILER_AUTH_USER,
-			pass: process.env.NODEMAILER_AUTH_PASS,
-		},
-		// debug: true, // show debug output
-		// logger: true, // log information in console
-	});
+const transporter = nodemailer.createTransport({
+	service: "gmail",
+	auth: {
+		user: process.env.NODEMAILER_AUTH_USER,
+		pass: process.env.NODEMAILER_AUTH_PASS,
+	},
+	// debug: true, // show debug output
+	// logger: true, // log information in console
+});
 
-	const mailOptions = {
+const sendWelcomeEmail = (email, name) => {
+	console.log("Sending welcome email...");
+
+	const welcomeMailOption = {
 		from: process.env.NODEMAILER_FROM,
-		to: "destinationemail@mail.com",
-		subject: "Test abs once again",
-		// text: "That was easy!",
-		html: "<h1>Welcome</h1><p>That was easy! test</p>",
+		to: email,
+		subject: "Thanks for joining in!",
+		// text:  `Welcome to the app, ${name}. Let me know how you get along with the app.`,
+		html: `<h1>Welcome!</h1><p>Welcome to the app, ${name}.</p><p>Let me know how you get along with the app.</p>`,
 	};
 
-	transporter.sendMail(mailOptions, function (error, info) {
+	transporter.sendMail(welcomeMailOption, function (error, info) {
 		if (error) {
 			console.log(error);
 		} else {
@@ -30,6 +31,25 @@ const sendMail = () => {
 	});
 };
 
-sendMail();
+const sendCancelationEmail = (email, name) => {
+	console.log("Sending cancelation email...");
 
-module.exports = sendMail;
+	const cancelationMailOption = {
+		from: process.env.NODEMAILER_FROM,
+		to: email,
+		subject: "Sorry to see you go!",
+		// text:  `Goodbye, ${name}. I hope to see you back sometime soon.`,
+		html: `<h1>Goodbye</h1><p>Goodbye, ${name}.</p><p>I hope to see you back sometime soon.</p>`,
+	};
+
+	transporter.sendMail(cancelationMailOption, function (error, info) {
+		if (error) {
+			console.log(error);
+		} else {
+			console.log(info.response);
+			console.log("Message sent: %s", info.messageId);
+		}
+	});
+};
+
+module.exports = { sendWelcomeEmail, sendCancelationEmail };
